bf7be3203245b263673bcd85f6f6028c1c8a5cc6
student <you@example.com>
2023-03-31 21:14:47 +0300

    second commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,14 @@ fun main() {
    println(returnList(listOf("Umbrella", "Car", "Pen", "Book", "Bag", "Jacket", "Sweater", "Door", " Wallet", "Soap")))
    println(average(listOf(5.45, 6.2, 1.76, 3.34)))
     sortedInfo()
-
+    pushNewInfo()
+   println( averageMileage())
 
 
 
 
 }
+//Question 1
 //Given a list of 10 strings, write a function that returns a list of the strings
 //at even indices i.e index 2,4,6 etc
 fun returnList (words: List<String>): List<String>{
@@ -19,6 +21,7 @@ fun returnList (words: List<String>): List<String>{
         }
     return emptyList
 }
+//Question 2
 //Given a list of people’s heights in metres. Write a function that returns
 //the average height and the total height
 data class PeopleHeights ( var average: Double, var total: Double)
@@ -29,11 +32,11 @@ data class PeopleHeights ( var average: Double, var total: Double)
        return info
 
 }
-
+//Question 3
 //Given a list of Person objects, each with the attributes, name, age,
 //height and weight. Sort the list in order of descending age
 data class Personobjects( var name: String, var age: Byte, var height: Double, var weight: Double)
-open fun sortedInfo(){
+fun sortedInfo(){
     val human = Personobjects("Joshua", 20, 1.80, 70.00)
     val human2 = Personobjects("Prince", 25, 1.73, 60.00)
     val human3 = Personobjects("Christelle", 28, 1.50, 56.02)
@@ -42,15 +45,40 @@ open fun sortedInfo(){
     println(sortedinfo)
 
 }
+//Question 4
 //Given a list similar to the one above, write a function in which you will
 //create 2 more people objects and add them to the list at one go.
-fun pushNewInfo(){
+fun pushNewInfo() {
     val human4 = Personobjects("Gilbert", 30, 1.75, 80.03)
     val human5 = Personobjects("Yannis", 19, 1.70, 90.05)
     val amigos = listOf(human4, human5)
+    val people = mutableListOf<Personobjects>()
+    people.addAll(amigos)
+    println(people)
+
 
 }
-
+//Question 5
 //Write a function that takes in a list of Car objects each with a
 //registration and mileage attribute and returns the average mileage of
 //all the vehicles in the list.
+data class CarObjects(var registration:String, var mileage:Double)
+fun averageMileage():Double{
+    val car1=CarObjects("KDC831A",240.60)
+    val car2=CarObjects("KDD332C",600.30)
+    val car3=CarObjects("KQD441D",800.19)
+    val car4=CarObjects("KAC",309.00)
+    var cars= listOf(car1,car2,car3,car4)
+    var sumOfMileage=cars.sumOf { Mile-> Mile.mileage }
+    var average=sumOfMileage/cars.size
+    return average
+}
+
+
+
+
+
+
+
+
+
